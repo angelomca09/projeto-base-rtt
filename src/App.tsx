cd5bcf0692dc3f6f71c2581545fbb21a3fc2102a
Angelo Munhoz <angelomca09@gmail.com>
2022-07-22 18:13:47 -0300

    Commit Inicial

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+export default function App() {
+  console.log('Hello Console!');
+
+  return (
+    <div>
+      <header>
+        <div className="bg-blue-500 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl">
+            Projeto base React / TypeScript
+          </h1>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mx-auto p-4">
+          <h2>Hello World!</h2>
+        </div>
+      </main>
+    </div>
+  );
+}
